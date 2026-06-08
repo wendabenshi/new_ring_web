4d44c803b9ba1a39d72021aa41582acf3a7c1b08
wenzr <wenzr@starspulse.com>
2026-06-08 15:25:22 +0800

    Revert Supabase env config change

--- src/supabase/index.js
+++ src/supabase/index.js
@@ -1,9 +1,8 @@
-// src/supabase.js
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = 'https://nbyahizzsymhmoltutlb.supabase.co'
-const supabaseAnonKey = 'sb_publishable_uT3f1hvo9JmlzNEMU-6r7Q_OMpI1RYD'
+const supabasePublishableKey = 'sb_publishable_uT3f1hvo9JmlzNEMU-6r7Q_OMpI1RYD'
 
-const supabase = createClient(supabaseUrl, supabaseAnonKey)
+const supabase = createClient(supabaseUrl, supabasePublishableKey)
 
 export default supabase
